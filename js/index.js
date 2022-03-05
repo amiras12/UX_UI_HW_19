384eda11ceefeffe79bc71dad7e3e43845aa5b40
amiras12 <amira.shotwell@gmail.com>
2022-03-05 09:52:41 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-console.log("Your index.js file is loaded correctly!");
+[[ $( document ).ready(function() {
+  console.log("ready!");
+})]];
 $('#button2').on("click", function(){
-    $('.cssChange').css("background-color","red");
+    $('.cssChange').css("background-color","white");
   });
