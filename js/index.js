b985a97a6e766eee09c2eed27b53842587e138c2
amiras12 <amira.shotwell@gmail.com>
2022-03-05 11:50:17 -0600

    3/5 update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-[[ $( document ).ready(function() {
-  console.log("ready!");
-})]];
-$('#button2').on("click", function(){
-    $('.cssChange').css("background-color","white");
-  });
+$(document).ready(function() {
+  console.log( "ready!" );
+  {
+      alert("Hello! I am an alert box!");
+    }
+  $('#button2').on("click", function(){
+          $('.cssChange').css("background-color","white");
+          console.log("button #2 was pressed!")
+        });
+});
